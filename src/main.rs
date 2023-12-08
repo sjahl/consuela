8b88e3a3ede477291e9548b3d7000c1c2e4341ee
sjahl <636687+sjahl@users.noreply.github.com>
2023-12-08 15:01:07 -0500

    oh my god something that works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use chrono::offset::Utc;
 use chrono::DateTime;
 use std::collections::HashMap;
-use std::fs::read_dir;
+use std::fs::{create_dir, read_dir, rename};
 use std::io;
+use std::path::Path;
 use std::time::SystemTime;
 
 // Represents a file and creation date
@@ -44,13 +45,13 @@ fn main() {
     // Build a list of DatedFiles
     // Create all the possible date directories
     // Move each file into its dated folder
-    let files_list = match dir_listing("/tmp/test-consuela") {
+    let root_folder = Path::new("/tmp/test-consuela"); // TODO: make this a cli arg
+    let files_list = match dir_listing(&root_folder.display().to_string()) {
         Ok(v) => v,
         Err(err) => panic!("error: {}", err),
     };
 
     let mut date_groups = HashMap::new();
-
     for item in files_list {
         date_groups
             .entry(item.ctime)
@@ -59,7 +60,17 @@ fn main() {
     }
 
     for (k, v) in &date_groups {
-        println!("{k:?} has {v:?}");
+        let full_fp = root_folder.join(k);
+        println!("{:?}", full_fp);
+        let _ = create_dir(&full_fp);
+        println!("will move {v:?} into {k:?}");
+        for filename in v {
+            println!("{:?}", filename);
+            let fuck = Path::new(filename).file_name().unwrap();
+            let target = &full_fp.join(fuck);
+            println!("{:?}", target);
+            let _ = rename(filename, target);
+        }
     }
 
     println!("No .... lemon pledge");
